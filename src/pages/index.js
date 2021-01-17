23d2676a1d2d34cfd848ee6767267989809b025f
Elynn <elynnlee@utexas.edu>
2021-01-17 13:23:27 -0800

    update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,71 +1,29 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import Helmet from 'react-helmet'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
-import Bio from '../components/Bio'
-import { rhythm } from '../utils/typography'
-import quoraIcon from '../images/svg-icons/quora.svg'
-import linkedinIcon from '../images/svg-icons/linkedin.svg'
-import instagramIcon from '../images/svg-icons/instagram.svg'
-
-const ListLink = props =>
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>
-      {props.children}
-    </Link>
-  </li>
-
-class BlogIndex extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const posts = get(this, 'props.data.allMarkdownRemark.edges')
-
-    return (
-      <div>
-        <Helmet title={siteTitle} />
-        <Bio />
-        <ul style={{ listStyle: `none`, float: `right` }}>
-          <a href="https://www.quora.com/profile/Elynn-Lee" style={{backgroundImage:"none"}}>
-            <span>
-              <img
-                src={quoraIcon}
-                style={{
-                  marginRight: rhythm(1 / 2),
-                  width: rhythm(1),
-                  height: rhythm (1)
-                }}
-              />
-            </span>
-          </a>
-          <a href="https://www.linkedin.com/in/elynnlee" style={{backgroundImage:"none"}}>
-            <span>
-              <img
-                src={linkedinIcon}
-                style={{
-                  marginRight: rhythm(1 / 2),
-                  width: rhythm(1),
-                  height: rhythm (1)
-                }}
-              />
-            </span>
-          </a>
-          <a href="https://www.instagram.com/elynnimator" style={{backgroundImage:"none"}}>
-           <span>
-              <img
-                src={instagramIcon}
-                style={{
-                  width: rhythm(1),
-                  height: rhythm (1)
-                }}
-              />
-            </span>
-          </a>
-        </ul>
-      </div>
-    )
-  }
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I'm currently a Product Manager at Opendoor.
+        </p>
+        <p>
+          Previously, I was at Quora as a PM and I've interned at Facebook,
+          Amazon, and Google as a Software Engineering Intern.
+           I graduated from The University of Texas at Austin in 2015
+           with an MS and BS in Computer Science.
+        </p>
+        <p>
+          You can find me on <a href="https://www.linkedin.com/in/elynnlee/">LinkedIn</a>, {' '}
+          <a href="https://www.quora.com/profile/Elynn-Lee">Quora</a>,
+          and, in my free time, <a href="https://www.instagram.com/elynnimator/">Instagram</a>.
+        </p>
+      </section>
+    </Layout>
+  )
 }
-
-export default BlogIndex
-
