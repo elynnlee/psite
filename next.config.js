974a2ba408867d463ce5c09d3b8d7b39eb114b6b
Elynn <elynnlee@utexas.edu>
2021-01-17 15:45:54 -0800

    try another next.config.js setup

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: isProd ? 'https://elynnlee.com' : '',
-}
+  assetPrefix: !debug ? '/psite' : ''
+};
