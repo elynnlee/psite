56103d75fb1c9fcd6a6e8241f901d10d396d3bd6
Elynn <elynnlee@utexas.edu>
2021-01-17 14:30:55 -0800

    set assetPrefix for github pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const ghPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 module.exports = {
-  assetPrefix: ghPages ? '/elynnlee.github.io/' : ''
+  assetPrefix: ghPages ? '/elynnlee.github.io/psite' : ''
 }
