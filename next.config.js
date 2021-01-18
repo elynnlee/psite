dea36e890d29dcd97796559f6d36d20ea8d33c3d
Elynn <elynnlee@utexas.edu>
2021-01-17 16:04:18 -0800

    next.config.js update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  assetPrefix: !debug ? '/psite' : ''
+  basePath: !debug ? '/psite' : '',
+  assetPrefix: !debug ? '/psite' : '',
 };
